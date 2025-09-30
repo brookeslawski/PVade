be9fd17190a25b05c03d179ea00f095fd2477ece
Brooke Stanislawski <brooke.stanislawski@gmail.com>
2025-09-30 13:00:12 -0600

    preparing to run all 4 cases at higher stiffness

--- run_scripts/launch_all_jobs.sh
+++ run_scripts/launch_all_jobs.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-for tracker_angle in 40.0 10.0 -10.0
+for tracker_angle in 40.0 -40.0 10.0 -10.0
 do
     # Modify input_h5_filename based on the tracker_angle value
     if [[ "${tracker_angle}" == "40.0" ]]; then
         input_h5_filename=input/pct_constrained_turb_ny154_nz38_sonic1_30s_u9.101_50Hz.h5
+    elif [[ "${tracker_angle}" == "-40.0" ]]; then
+        input_h5_filename=input/pct_constrained_turb_ny154_nz38_sonic1_30s_u8.127_50Hz.h5
     elif [[ "${tracker_angle}" == "-10.0" ]]; then
         input_h5_filename=input/pct_constrained_turb_ny154_nz38_sonic1_30s_u9.0_50Hz.h5
     elif [[ "${tracker_angle}" == "10.0" ]]; then
@@ -14,7 +16,7 @@ do
         continue
     fi
     
-    output_dir=/scratch/bstanisl/pvade/turb_inflow/output/halfwing_turbinflow_duramat_validation_${tracker_angle}deg/ \
+    output_dir=/scratch/bstanisl/pvade/turb_inflow/output/halfwing_stiffer_turbinflow_duramat_validation_${tracker_angle}deg/ \
     output_log=${output_dir}/log.txt
     sbatch --job-name=duramatval_${tracker_angle}deg \
            --output=${output_log} \
